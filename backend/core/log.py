70e4f25f764dbc93003bb11d3a794960299df6df
AllanWu <allanwu36@qq.com>
2024-04-12 10:31:56 +0800

    1、增加日志文件的处理

--- backend/core/log.py
+++ backend/core/log.py
@@ -1,15 +1,25 @@
 import logging
+from logging import handlers
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-# 创建一个 FileHandler，用于写入日志文件
-handler = logging.FileHandler('log_file.log', encoding="utf-8")
-handler.setLevel(logging.INFO)
-
-# 设置日志格式
+logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter("[%(asctime)s] %(levelname)s %(message)s")
-handler.setFormatter(formatter)
 
-# 添加 FileHandler 到日志处理器中
+#往屏幕上输出
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+#往文件里输出
+log_file = handlers.TimedRotatingFileHandler(filename="all.log",when="D",backupCount=10,encoding='utf-8')#往文件里写入#指定间隔时间自动生成文件的处理器
+#实例化TimedRotatingFileHandler
+        #backupCount是备份文件的个数，如果超过这个个数，就会自动删除，when是间隔的时间单位，单位有以下几种：
+        # S 秒
+        # M 分
+        # H 小时、
+        # D 天、
+        # W 每星期（interval==0时代表星期一）
+        # midnight 每天凌晨
+log_file.setFormatter(formatter)#设置文件里写入的格式
+logger.addHandler(log_file)
+
